95f9d5e296e866386388b7318cfcb69fe8ac5923
Allie <alliecaton3@gmail.com>
2020-12-15 13:01:58 -0500

    commit tree 5587b8c87321d0fb07ee871f4b29c039d73a3af0

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+
+class School 
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end 
+
+    def add_student (student_name, grade)
+      if @roster.has_key?(grade)
+            @roster[grade] << student_name
+      else
+        @roster[grade] = []
+        @roster[grade] << student_name
+      end
+    end 
+
+    def grade (grade)
+        @roster.collect do | key, value |
+            if key == grade 
+                value
+            end 
+        end.flatten.compact
+    end
+
+    def sort
+        @roster.collect do |key,value|
+            @roster[key] = []
+            @roster[key] = value.sort.flatten
+        end
+        @roster
+    end
+
+
+end 
